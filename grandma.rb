ec1d9789a9209925f176247919545c7c200ebed3
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-04-18 22:08:22 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(word)
-  
+  if word != word.upcase
+    "HUH?! SPEAK UP, SONNY"
+  elsif word == word.upcase
+    "I LOVE YOU TOO PUMPKIN"
+  end
+
 end
 
 
